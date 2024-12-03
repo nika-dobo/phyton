44f276c565b1b427cbdcb9c891235c6512ee1f07
nika-dobo <nikadobo20@gmail.com>
2024-12-03 15:11:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -152,15 +152,32 @@ print(found)
 print("")
 
 #list
-list = [5, 9, 56, 1, 2, 4, 6, 12]#ლისტში რაიმეს დამატება
-list.append(100)
+list = [5, 9, 56, 1, 2, 4, 6, 12]
+list.append(100)#ლისტში რაიმეს დამატება
 list.insert(1, 100) # insert-ის საშუალებით ვირჩევრ ინდექს და შემდეგ ვსვამთ რაც გვინდა. 1 არის ინდექსი ესეგი მერამდენი გვინდა იყო, 100 არის რაც გვინდა ჩავსვათ'
 list.extend([8, 4, 2, 63]) # extend რამოდენიმე რიცხვის ჩასმა ლისტში
 list.sort()# sort ალაგებს ლისტს ზრდადობით
 # list.reverse()#reverse ლისტს ატრიალებს
 list.pop()# შლის ბოლო ელემენტს
 list.remove(12)# წაშლის რასც მივუთითებთ
+# list.clear()# შლის ყველაფერს
 print(list)
+print(list.count(2))#count ითვლის რამდენია ამ ლისტში 2
+print(len(list))#ითვლის ლისტის მიგრძეს
+
+for i in list:
+    i *= 2
+    print(i)
 
 
+n = int(input("enter lenght: "))
 
+user_list = []
+
+i = 0
+while i < n:
+    string = "enter element  #" + str(i + 1) + ": "
+    user_list.append(input(string))
+    i += 1
+
+print(user_list)
